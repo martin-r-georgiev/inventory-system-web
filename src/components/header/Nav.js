b7d5bcd5b0313092495a471afee7b0bd5e2c81df
martin-r-georgiev <martin.r.georgiev@hotmail.com>
2021-01-06 12:36:19 +0200

    Added a chat button in the navigation bar

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -63,6 +63,9 @@ const Nav = ({user, setUser}) => {
                     : ''
                     }
                     <li className="nav-item ml-auto">
+                        <Link className="nav-link" to='/chat'>Chat</Link>
+                    </li>
+                    <li className="nav-item ml-auto">
                         <Link className="nav-link" onClick={handleLogout} to='/'>Log out</Link>
                     </li>
                 </ul>
